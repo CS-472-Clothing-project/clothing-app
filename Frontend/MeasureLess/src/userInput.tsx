bcc4f13678977aeb67753b48b2999f5808f9466b
devinjathan <devinjathan@gmail.com>
2025-10-16 17:24:56 -0700

    added scroll wheel feature and scroll library for user height

--- Frontend/MeasureLess/src/userInput.tsx
+++ Frontend/MeasureLess/src/userInput.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { FaMale, FaFemale, FaUser } from "react-icons/fa";
 import "./assets/react.svg";
+import Picker from 'react-mobile-picker';
 
 
 function UserInput() {
     const[bodyType, setBodyType] = useState("");
     //const[userFt, setUserFt] = useState("");
     //const[userIn, setUserIn] = useState("");
+    const selections = {
+        feet: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        inches: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
+    }
+    const [pickerValue, setPickerValue] = useState({
+        feet: "",
+        inches: "",
+    });
+
     return(
         <div className="flex flex-col">
             <h1>User Input</h1>
@@ -47,6 +57,25 @@ function UserInput() {
 
 
             <p>Current body Type: {bodyType || "not set"}</p>
+            <div className="border-black flex justify-center items-center">
+            <Picker value={pickerValue} onChange={setPickerValue} wheelMode={"natural"}>
+                <Picker.Column name={"feet"}>
+                    {selections.feet.map((ft) => (
+                        <Picker.Item key={ft} value={ft}>
+                            {ft} ft.
+                        </Picker.Item>
+                    ))}
+                </Picker.Column>
+                <Picker.Column name={"inches"}>
+                    {selections.inches.map((inch) => (
+                        <Picker.Item key={inch} value={inch}>
+                            {inch} in.
+                        </Picker.Item>
+                    ))}
+                </Picker.Column>
+            </Picker>
+            </div>
+            Selected height: <strong>{pickerValue.feet} ft {pickerValue.inches} in</strong>
         </div>
     );
 }
