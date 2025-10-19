807f0dd000b0aa58b2a910f38f468dc21e3cad10
Oscar Sanchez-Serna <sancho5@unlv.nevada.edu>
2025-10-18 19:28:21 -0700

    Initial attempt to have a file only be read once in ImageHandler

--- MeasureLess/modules/ImageHandler.py
+++ MeasureLess/modules/ImageHandler.py
@@ -20,11 +20,17 @@ class ImageHandler:
             # once for each framework. Should look into how to just
             # read once
             self.cvImage = cv2.imread(fileName)
-            self.mpImage = mp.Image.create_from_file(fileName)
+            # Change to only be read once, i.e. having opencv read in the image
+            # and simply convert 
+            tmp_image = cvImage.copy()
+            # OpenCV reads in images as BGR instead of RGB, so convert it
+            tmp_image = cv2.cvtColar(tmp_image, cv2.COLOR_BGR2RGB)
+            self.mpImage = mp.Image(image_format=mp.ImageFormat.SRGB, data=tmp_image)
+            # self.mpImage = mp.Image.create_from_file(fileName)
             self.ndArrayImage = self.mpImage.numpy_view()
             
         except:
-            print("Error loading image")
+            logging.exception("Error loading image")
         
     # Placeholder for later when checking for image type compatibility
     def checkImageType(self):
@@ -35,12 +41,12 @@ class ImageHandler:
         pass
     
     # For privacy concerns, check if the file exists
-    # If it does, delte it
+    # If it does, delete it
     def __del__(self):
         if(self.debug == False):
             # Using the os library, check if the image is present, if it is and debug is False
             # Delete the image
             if os.path.isfile(self.fileName):
                 os.remove(self.fileName)
-                print(f"File: {self.fileName} has been removed.")
+                print(f"Image: {self.fileName} has been removed.")
         return
