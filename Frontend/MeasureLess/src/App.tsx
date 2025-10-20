5cd0fd489133b9b4e0bc97b73afd334f3bf2c221
Matthew Lopez <lopez69@unlv.nevada.edu>
2025-10-20 00:05:10 -0700

    Output with results table with in/cm toggle, Save to profile (local), and CSV export

--- Frontend/MeasureLess/src/App.tsx
+++ Frontend/MeasureLess/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import UserInput from './userInput'
 import TakePicture from './takePicture.tsx'
-import Instructions from './instructions'   // ← ADD
+import Instructions from './instructions' 
+import Output from './output.tsx'
 
 function Home() {
   return <h1>Home Page</h1>
@@ -15,14 +16,16 @@ function App() {
         <Link to="/">Home</Link> |{" "}
         <Link to="/userInput.tsx">UserInput</Link> |{" "}
         <Link to="/takePicture.tsx">TakePicture</Link> |{" "}
-        <Link to="/instructions">Instructions</Link>  {/* ← ADD */}
+        <Link to="/instructions">Instructions</Link>  |{" "}
+        <Link to="/output">Output</Link> |{" "}
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/userInput.tsx" element={<UserInput />} />
         <Route path="/takePicture.tsx" element={<TakePicture />} />
-        <Route path="/instructions" element={<Instructions />} /> {/* ← ADD */}
+        <Route path="/instructions" element={<Instructions />} /> {" "}
+        <Route path="/output" element={<Output />} />
       </Routes>
     </BrowserRouter>
   )
