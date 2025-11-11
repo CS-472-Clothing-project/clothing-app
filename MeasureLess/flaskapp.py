2ab0857a42eeb0e39b70cd5a8bcaac5cabb1dfba
Matt MacQueston <Matt.MacQuest@gmail.com>
2025-11-11 13:33:41 -0800

    added flaskapp.py to start integrating flask backend with our application

---
+++ MeasureLess/flaskapp.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+directory_name = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(directory_name, 'uploads')
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(redirect.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return
