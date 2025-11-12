3977a2aa630857d2f4fec2e25a5c83917b388663
Matt MacQueston <Matt.MacQuest@gmail.com>
2025-11-12 15:18:34 -0800

    Added jsonify and CORS to enable receipt of image from react frontend

--- MeasureLess/flaskapp.py
+++ MeasureLess/flaskapp.py
@@ -1,14 +1,19 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 
 directory_name = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(directory_name, 'uploads')
+
+
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+CORS(app)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -18,14 +23,14 @@ def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return jsonify({'error': 'No file part'}), 400
         file = request.files['file']
         
         if file.filename == '':
             flash('No selected file')
-            return redirect(redirect.url)
+            return jsonify({'error': 'No selected file'}), 400
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return jsonify({'message': 'File uploaded successfully', 'filename': filename}), 200
     return
