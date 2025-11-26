d6d4a74fd831e6e6ba936d0f811f27a6bfc14973
devinjathan <devinjathan@gmail.com>
2025-11-26 15:37:57 -0800

    add handle functions to delete and save to profile

--- Frontend/MeasureLess/src/firebase/db.js
+++ Frontend/MeasureLess/src/firebase/db.js
@@ -1,3 +1,34 @@
-import { doc, setdoc } from "firebase/firestore";
+import { doc, setDoc, updateDoc, deleteField } from "firebase/firestore";
 import { auth, db } from "./firebase.js";
 
+// save ONE measurement set to profile
+export const doSaveToProfile = async (measurements: any) => {
+    const user = auth.currentUser;
+    // if not logged in
+    if (!user) throw new Error("No auth");
+    // if guest user
+    if (user.isAnonymous) {
+        throw new Error("Must Sign in");
+    }
+
+    const userRef = doc(db, "users", user.uid);
+
+    //merges with past (change for multiple please)
+    await setDoc(userRef, { measurements }, { merge: true });
+};
+
+export const deleteFromProfile = async () => {
+    const user = auth.currentUser;
+    // if not logged in
+    if (!user) throw new Error("No auth");
+    // if guest user
+    if (user.isAnonymous) {
+        throw new Error("Must Sign in");
+    }
+
+    const userRef = doc(db, "users", user.uid);
+
+    //merges with past (change for multiple please)
+    await updateDoc(userRef, { measurements: deleteField() });
+
+}
