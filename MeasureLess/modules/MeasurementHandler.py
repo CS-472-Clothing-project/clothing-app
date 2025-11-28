3bd669536e3a6fdacea726e4e5b78822731794aa
CristianNativo <nativoga@unlv.nevada.edu>
2025-11-28 13:05:45 -0800

    Changed how getPointFromBackground works by ending at segemented_image.shape - 1 to avoid an out of bounds error.

--- MeasureLess/modules/MeasurementHandler.py
+++ MeasureLess/modules/MeasurementHandler.py
@@ -130,7 +130,7 @@ class MeasurementHandler:
         x,y = point
 
         if direction == "right":
-            for x_pixel in range(x, segmented_image.shape[1], 1):
+            for x_pixel in range(x, segmented_image.shape[1]-1, 1):
                 temp = (x_pixel, y)
                 if self.checkBackground(temp, segmented_image, direction):
                     return temp
@@ -148,7 +148,7 @@ class MeasurementHandler:
                     return temp
 
         elif direction == "down":
-            for y_pixel in range(y, segmented_image.shape[0], 1):
+            for y_pixel in range(y, segmented_image.shape[0]-1, 1):
                 temp = (x, y_pixel)
                 if self.checkBackground(temp, segmented_image, direction):
                     return temp
