3a5c8459ca4b4d366214cb6507976d732473ebfc
Jesse <99794044+ColonJesseJ@users.noreply.github.com>
2025-12-01 23:12:36 -0800

    vite config try

--- Frontend/MeasureLess/vite.config.ts
+++ Frontend/MeasureLess/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
@@ -12,9 +12,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        hmr: {
-            host: "clothing-app-2n6a.onrender.com",
-        },
+        port: 5173,
     },
-});
+}));
 
